2e256424749a584230f6f656ea050834f89cad23
Justin René Back <jback@pixelcatproductions.net>
2021-02-07 07:58:45 +0100

    Add urlshortened links

--- src/commands/bugreport.ts
+++ src/commands/bugreport.ts
@@ -12,12 +12,12 @@ module.exports = {
             .setColor('#0099ff')
             .setTitle("Bugreport")
             .setDescription(`To submit a bug report, use the links below`)
-            .addField("Phoenix (edit.tosdr.org)", "[Report a bug](https://github.com/tosdr/edit.tosdr.org/issues/new?labels=bug)\n[Suggest a feature](https://github.com/tosdr/edit.tosdr.org/issues/new?labels=request)")
-            .addField("Crisp (tosdr.org)", "[Report a bug](https://github.com/tosdr/CrispCMS/issues/new?labels=bug)\n[Suggest a feature](https://github.com/tosdr/CrispCMS/discussions/new?category=ideas)")
-            .addField("Phoenix Crawler (crawler.tosdr.org)", "[Report a bug](https://github.com/tosdr/crawler.tosdr.org/issues/new?labels=bug)\n[Suggest a feature](https://github.com/tosdr/crawler.tosdr.org/discussions/new?category=ideas)")
-            .addField("Browser Extensions", "[Report a general bug](https://github.com/tosdr/browser-extensions/issues/new?labels=bug)\n---\n[Report a bug for Firefox](https://github.com/tosdr/browser-extensions/issues/new?assignees=&labels=bug,firefox&template=mozilla-firefox.md&title=)\n[Report a bug for Edge](https://github.com/tosdr/browser-extensions/issues/new?assignees=&labels=bug,edge&template=microsoft-edge.md&title=)\n[Report a bug for Opera](https://github.com/tosdr/browser-extensions/issues/new?assignees=&labels=bug%2C+opera&template=opera.md&title=)\n[Report a bug for Chrome](https://github.com/tosdr/browser-extensions/issues/new?assignees=&labels=bug%2C+chrome&template=google-chrome.md&title=)\n[Report a bug for Safari](https://github.com/tosdr/browser-extensions/issues/new?assignees=&labels=bug%2C+safari&template=safari.md&title=)\n---\n[Suggest a feature](https://github.com/tosdr/browser-extensions/discussions/new?category=ideas)")
-            .addField("Me! T.O.D.D", "[Report a bug](https://github.com/JustinBack/TODD-Discord/issues/new?labels=bug)\n[Suggest a feature](https://github.com/JustinBack/TODD-Discord/discussions/new?category=ideas)")
-            .addField("DocBot", "[Report a bug](https://github.com/JustinBack/DocBot-Server/issues/new?labels=bug)\n[Suggest a feature](https://github.com/JustinBack/DocBot-Server/discussions/new?category=ideas)")
+            .addField("Phoenix (edit.tosdr.org)", "[Report a bug](https://to.tosdr.org/phoenix-bug-report)\n[Suggest a feature](https://to.tosdr.org/phoenix-feature)")
+            .addField("Crisp (tosdr.org)", "[Report a bug](https://to.tosdr.org/crisp-bug-report)\n[Suggest a feature](https://to.tosdr.org/crisp-feature)")
+            .addField("Phoenix Crawler (crawler.tosdr.org)", "[Report a bug](https://to.tosdr.org/crawler-bug-report)\n[Suggest a feature](https://to.tosdr.org/crawler-feature)")
+            .addField("Browser Extensions", "[Report a general bug](https://to.tosdr.org/extension-general-bug)\n---\n[Report a bug for Firefox](https://to.tosdr.org/extension-firefox-bug)\n[Report a bug for Edge](https://to.tosdr.org/extension-edge-bug)\n[Report a bug for Opera](https://to.tosdr.org/extension-opera-bug)\n[Report a bug for Chrome](https://to.tosdr.org/extension-chrome-bug)\n[Report a bug for Safari](https://to.tosdr.org/extension-safari-bug)\n---\n[Suggest a feature](https://to.tosdr.org/extension-feature)")
+            .addField("Me! T.O.D.D", "[Report a bug](https://to.tosdr.org/todd-bug-report)\n[Suggest a feature](https://to.tosdr.org/todd-feature)")
+            .addField("DocBot", "[Report a bug](https://to.tosdr.org/docbot-bug-report)\n[Suggest a feature](https://to.tosdr.org/docbot-feature)")
             .setTimestamp();
 
         message.message.channel.send(embed).then(() => {
