09758d16f3e5bd1f3c9265e13424bfd0b0674ff2
Justin Back <jback@pixelcatproductions.net>
2021-03-13 23:07:36 +0100

    remove comment

--- src/utils/bitmask-names.ts
+++ src/utils/bitmask-names.ts
@@ -1,13 +1,9 @@
 import { Permissions } from '../models';
 
-/**
- * Load all commands in the `commands/` folder and return a collection of
- * commands.
- */
 export function bitmaskNames(Bitmask: any) {
-    let _arr:any = [];
-    for(var i in Permissions){
-        if(Bitmask & Permissions[i]){
+    let _arr: any = [];
+    for (var i in Permissions) {
+        if (Bitmask & Permissions[i]) {
             _arr.push(i);
         }
     }
